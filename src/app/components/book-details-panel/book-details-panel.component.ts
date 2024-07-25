84caaecf2eebc86f44d03cec209f49aee67e66f5
Rigoberto Leander Salgado Reyes <rlsalgado2006@gmail.com>
2024-07-24 19:51:52 -0500

    adding download feature

--- src/app/components/book-details-panel/book-details-panel.component.ts
+++ src/app/components/book-details-panel/book-details-panel.component.ts
@@ -7,6 +7,10 @@ import { SubjectPipe } from "(src)/app/pipes/subject.pipe";
 import { AsyncPipe, NgIf, NgOptimizedImage } from "@angular/common";
 import { FileCheckService } from "(src)/app/services/file-check.service";
 import { catchError, from, Observable, of } from "rxjs";
+import { DownloadService } from "(src)/app/services/download.service";
+import { HttpErrorResponse } from "@angular/common/http";
+
+declare var bootstrap: any;
 
 @Component({
 	selector: "book-details-panel",
@@ -25,8 +29,10 @@ import { catchError, from, Observable, of } from "rxjs";
 })
 export class BookDetailsPanelComponent {
 	@Input() file!: File;
+	downloadMessage: string = "";
+	titleMessage: string = "Information";
 
-	constructor(private fileCheckService: FileCheckService) {}
+	constructor(private fileCheckService: FileCheckService, private downloadService: DownloadService) {}
 
 	checkFileExists(file: File): Observable<boolean> {
 		return from(this.fileCheckService.checkFileExists(this.getCoverId(file)))
@@ -36,5 +42,59 @@ export class BookDetailsPanelComponent {
 	getCoverId(file: File): string {
 		const coverId = `${file.webDetails?.cover_i ?? "no-cover"}`;
 		return !file.customDetails ? file.coverId : coverId;
+	}
+
+	onDownload(file: { parentPath: string; name: string }) {
+		const fileUrl = `${file.parentPath}/${file.name}`;
+		this.downloadService.downloadFile(fileUrl).pipe(
+			catchError((error: HttpErrorResponse) => {
+				this.downloadMessage = this.getErrorMessage(error);
+				console.info(`${this.titleMessage} - ${this.downloadMessage}`);
+				this.messageDialog();
+				return of(null);
+			})
+		).subscribe({
+			next: (blob) => {
+				if (blob) {
+					const url = window.URL.createObjectURL(blob);
+					const a = document.createElement("a");
+					a.href = url;
+					a.download = file.name;
+					a.click();
+					window.URL.revokeObjectURL(url);
+					console.info("Download book", fileUrl);
+				}
+			},
+			error: (error) => {
+				console.error("Download failed", error);
+			},
+			complete: () => {
+				console.info("Download process completed.");
+			}
+		});
+	}
+
+	private getErrorMessage(error: HttpErrorResponse): string {
+		if (error.error instanceof ErrorEvent) {
+			// Error del lado del cliente o de red
+			return `Error: ${error.error.message}`;
+		} else {
+			// Error del lado del servidor
+			this.titleMessage = `Error ${error.status}`;
+			return `${error.statusText} URL: ${decodeURIComponent(error.url ?? "")}`;
+		}
+	}
+
+	clearDownloadMessage() {
+		this.downloadMessage = "";
+		this.titleMessage = "Information";
+	}
+
+	messageDialog() {
+		const modalElement = document.getElementById("downloadConfirmationModal");
+		if (modalElement) {
+			const modal = new bootstrap.Modal(modalElement);
+			modal.show();
+		}
 	}
 }
