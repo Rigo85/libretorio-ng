c5455b11a14c5fd97183d7815ded93eabc039540
Rigoberto Leander Salgado Reyes <rlsalgado2006@gmail.com>
2024-11-18 14:23:11 -0500

    updated formatTime

--- src/app/components/audiobook-viewer/audiobook-viewer.component.ts
+++ src/app/components/audiobook-viewer/audiobook-viewer.component.ts
@@ -56,7 +56,7 @@ export class AudiobookViewerComponent implements OnChanges, OnInit, OnDestroy {
 			this.currentTrackIndex = 0;
 			this.currentTrack = this.playlist[this.currentTrackIndex];
 			this.audio.src = this.currentTrack.src;
-			console.info(this.currentTrack.src);
+			// console.info(this.currentTrack.src);
 			this.spinner.hide().catch(console.error);
 
 			this.scrollTop();
@@ -170,9 +170,20 @@ export class AudiobookViewerComponent implements OnChanges, OnInit, OnDestroy {
 	}
 
 	formatTime(seconds: number): string {
-		const minutes = Math.floor(seconds / 60);
-		const secs = Math.floor(seconds % 60);
-		return `${minutes}:${secs < 10 ? "0" : ""}${secs}`;
+		// const minutes = Math.floor(seconds / 60);
+		// const secs = Math.floor(seconds % 60);
+		// return `${minutes}:${secs < 10 ? "0" : ""}${secs}`;
+		if (isNaN(seconds) || !seconds) return "0:00";
+
+		const h = Math.floor(seconds / 3600);
+		const m = Math.floor((seconds % 3600) / 60);
+		const s = Math.round(seconds % 60);
+
+		const hours: string = h > 0 ? String(h).padStart(2, "0") + ":" : "";
+		const minutes: string = (h > 0 || m > 0) ? String(m).padStart(2, "0") + ":" : "";
+		const secs = String(s).padStart(2, "0");
+
+		return hours + minutes + secs;
 	}
 
 	private scrollTop() {
